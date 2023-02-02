388da82137de2736afa577f526f6f4e8fe114efa
Rizwan Rasheed <49282604+rizwanrasheedsohal@users.noreply.github.com>
2023-02-02 15:33:31 +0500

    Create all_events_button.js

---
+++ all_events_button.js
@@ -0,0 +1,12 @@
+
+ // Create a button element
+ const button = document.createElement('button')
+ const anchor = document.createElement('a')
+ const teamId = document.getElementById("trackhero-embed-button-script").getAttribute("team_id");
+
+ button.innerText = 'Book Now'
+ anchor.id = 'trackhero-all-events-button'
+ anchor.appendChild(button); 
+ anchor.href=`http://localhost:3001/skipbarber?team_id=${teamId}`;
+
+ document.body.appendChild(anchor) 
