0a891a7b5d5e5ffcb4d3d6e875ef3a9a07f2d182
Rizwan Rasheed <49282604+rizwanrasheedsohal@users.noreply.github.com>
2023-02-02 14:03:14 +0500

    Update specific_events_button.js

--- specific_events_button.js
+++ specific_events_button.js
@@ -2,11 +2,11 @@
  // Create a button element
  const button = document.createElement('button')
  const anchor = document.createElement('a')
- const teamId = document.getElementById("trackhero-embed-button-script").getAttribute("team_id");
- const eventCategories = document.getElementById("trackhero-embed-button-script").getAttribute("cat_ids");
+//  const teamId = document.getElementById("trackhero-embed-button-script").getAttribute("team_id");
+//  const eventCategories = document.getElementById("trackhero-embed-button-script").getAttribute("cat_ids");
 
  button.innerText = 'Book Now'
  anchor.appendChild(button); 
- anchor.href=`http://localhost:3001/skipbarber?team_id=${teamId}&cat_ids=${eventCategories}`;
+ anchor.href=`http://localhost:3001/skipbarber?team_id=${1}&cat_ids=${}`;
 
  document.body.appendChild(anchor) 
